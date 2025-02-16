99fa84985df26b7cda919444b4c3d2fe949c6c99
samcharles5 <sc240843@gmail.com>
2025-02-16 17:04:31 +0530

    portfolio added + hosted

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
        <div style={{ position: "relative", zIndex: 1 }}>
         <Weather />
       </div>
+      <div className="position-fixed top-0 end-0 translate-middle-x p-3 anker m-5"> <b>Check out my portfolio <a href="https://samcharles-sde.web.app/">Link</a></b></div>
     </div>
   );
 }
